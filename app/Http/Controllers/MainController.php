26e03c07128381eea613b9d2794f1a7d67c59536
Hadson Roque <hadson.roque01@git config --global user.email hadson.roque01@gmail.com>
2023-02-03 13:58:12 +0100

    Create function to print in 'home.blade.php'

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -4,7 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Saint;
+
 class MainController extends Controller
 {
-    //
+    public function home()
+    {
+        $saints = Saint::all();
+
+        $data = [
+            'saints' => $saints
+        ];
+        return view('home', $data);
+    }
 }
